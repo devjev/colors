700d1dc3852617c93ab260299d364ed4ce35ce2a
Jevgeni Tarassov <jevgeni@tarassov.ch>
2023-12-24 21:37:59 +0100

    Fix HEX conversion

--- src/conversion/module.zig
+++ src/conversion/module.zig
@@ -1,3 +1,7 @@
+const srgb_hex = @import("srgb<->hex.zig");
+pub const srgb_to_hex = srgb_hex.srgb_to_hex;
+pub const hex_to_srgb = srgb_hex.hex_to_srgb;
+
 const srgb_xyz = @import("srgb<->xyz.zig");
 pub const srgb_to_xyz = srgb_xyz.srgb_to_xyz;
 pub const xyz_to_srgb = srgb_xyz.xyz_to_srgb;
--- src/conversion/srgb<->hex.zig
+++ src/conversion/srgb<->hex.zig
@@ -8,23 +8,41 @@ const HexCode = [6:0]u8;
 pub fn srgb_to_hex(
     comptime float_t: type,
     srgb: *const structs.sRGB(float_t),
-) errors.ColorsError!HexCode {
+) errors.ColorError!HexCode {
     comptime assert.chunky_float(float_t);
 
     if (!srgb.is_valid()) {
         return errors.ColorError.RGBColorValuesInvalid;
     }
 
-    const red_int = @as(u8, srgb.*.red * 255.0);
-    const green_int = @as(u8, srgb.*.green * 255.0);
-    const blue_int = @as(u8, srgb.*.blue * 255.0);
+    const red_int: u8 = @intFromFloat(srgb.red * 255.0);
+    const green_int: u8 = @intFromFloat(srgb.green * 255.0);
+    const blue_int: u8 = @intFromFloat(srgb.blue * 255.0);
 
-    var buf: HexCode = undefined;
-    std.fmt.bufPrint(buf[0..2], "{X}", .{red_int});
-    std.fmt.bufPrint(buf[2..4], "{X}", .{green_int});
-    std.fmt.bufPrint(buf[4..6], "{X}", .{blue_int});
+    var result: HexCode = undefined;
 
-    return buf;
+    var red_code: [2]u8 = undefined;
+    var green_code: [2]u8 = undefined;
+    var blue_code: [2]u8 = undefined;
+
+    _ = std.fmt.bufPrint(&red_code, "{X:0>2}", .{red_int}) catch {
+        return errors.ColorError.HexColorCodeInvalid; // TODO error could be better
+    };
+    _ = std.fmt.bufPrint(&green_code, "{X:0>2}", .{green_int}) catch {
+        return errors.ColorError.HexColorCodeInvalid;
+    };
+    _ = std.fmt.bufPrint(&blue_code, "{X:0>2}", .{blue_int}) catch {
+        return errors.ColorError.HexColorCodeInvalid;
+    };
+
+    result[0] = red_code[0];
+    result[1] = red_code[1];
+    result[2] = green_code[0];
+    result[3] = green_code[1];
+    result[4] = blue_code[0];
+    result[5] = blue_code[1];
+
+    return result;
 }
 
 pub fn hex_to_srgb(
@@ -34,21 +52,21 @@ pub fn hex_to_srgb(
     comptime assert.chunky_float(float_t);
 
     const red_code = hex[0..2];
-    const red_255_u8 = try std.fmt.parseInt(u8, red_code, 16) catch {
+    const red_255_u8 = std.fmt.parseInt(u8, red_code, 16) catch {
         return error.HexColorCodeInvalid;
     };
     const red_255_f: float_t = @floatFromInt(red_255_u8);
     const red: float_t = red_255_f / 255.0;
 
     const green_code = hex[2..4];
-    const green_255_u8 = try std.fmt.parseInt(u8, green_code, 16) catch {
+    const green_255_u8 = std.fmt.parseInt(u8, green_code, 16) catch {
         return error.HexColorCodeInvalid;
     };
     const green_255_f: float_t = @floatFromInt(green_255_u8);
     const green: float_t = green_255_f / 255.0;
 
     const blue_code = hex[4..6];
-    const blue_255_u8 = try std.fmt.parseInt(u8, blue_code, 16) catch {
+    const blue_255_u8 = std.fmt.parseInt(u8, blue_code, 16) catch {
         return error.HexColorCodeInvalid;
     };
     const blue_255_f: float_t = @floatFromInt(blue_255_u8);
@@ -62,7 +80,7 @@ pub fn hex_to_srgb(
 }
 
 test "Yellow: sRGB -> hex code" {
-    const expected: HexCode = "FFD700";
+    const expected: HexCode = "FFD700".*;
     const yellow_xyz = structs.sRGB(f32){
         .red = 1.0,
         .green = 0.8431372549,
@@ -71,12 +89,12 @@ test "Yellow: sRGB -> hex code" {
 
     const got = try srgb_to_hex(f32, &yellow_xyz);
 
-    std.testing.expectEqual(expected, got);
+    try std.testing.expectEqual(expected, got);
 }
 
 test "Yello: HEX -> sRGB" {
     const utils = @import("../testing-utils.zig");
-    const source: HexCode = "FFD700";
+    const source: HexCode = "FFD700".*;
     const yellow_srgb = structs.sRGB(f32){
         .red = 1.0,
         .green = 0.8431372549,
